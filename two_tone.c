a6d81792f64ef6d1a92d45da01e88bedaea24d87
MicroPhonon <mph@microphonon.com>
2020-01-30 00:47:04 -0500

    Update two_tone.c

--- two_tone.c
+++ two_tone.c
@@ -1,21 +1,24 @@
 #include <msp430.h> 
 /*
-Use the MSP430FR5969 Launchpad to play a repetitive two-tone sequence through an external piezoelectric buzzer. Square waves are generated at 2200 and 1300 Hz, which are the maritime radiotelephone distress frequencies. Tone duration is set by timer TA0 sourced to the VLO clock at 10 kHz. Tone frequency is set by TA1 sourced to SMCLK at 500 kHz. Drive the buzzer with TA1.1 output on P1.2. Use the TA0 interrupt to change the frequency.
+Use the MSP430FR5969 Launchpad to play a repetitive two-tone sequence through an external piezoelectric buzzer. Square 
+waves are generated at 2200 and 1300 Hz, which are the maritime radiotelephone distress frequencies. Tone duration is 
+set by timer TA0 sourced to the VLO clock at 10 kHz. Tone frequency is set by TA1 sourced to SMCLK at 500 kHz. Drive 
+the buzzer with TA1.1 output on P1.2. Use the TA0 interrupt to change the frequency.
 */
 void main(void) {
 
     	WDTCTL = WDTPW | WDTHOLD;   //Stop watchdog timer 	
-	    P1DIR |= BIT0 + BIT1 + BIT2 + BIT3 + BIT4 + BIT5 + BIT6 + BIT7; //Set P1 pins to outputs
+	P1DIR |= BIT0 + BIT1 + BIT2 + BIT3 + BIT4 + BIT5 + BIT6 + BIT7; //Set P1 pins to outputs
         //Setup TA1.1 output on P1.2
         P1SEL0 |= BIT2;
         P1SEL1 &= ~BIT2;
- 	    PM5CTL0 &= ~LOCKLPM5; //Unlock the configured GPIO pins
+ 	PM5CTL0 &= ~LOCKLPM5; //Unlock the configured GPIO pins
 
-	    CSCTL0 = CSKEY; //Password to unlock the clock registers
+	CSCTL0 = CSKEY; //Password to unlock the clock registers
      	CSCTL2 |= SELA__VLOCLK;  //Set ACLK to VLO with default frequency ~ 10 kHz; SMCLK default 1 MHz
      	CSCTL0_H = 0xFF; //Re-lock the clock registers; probably optional
 
-	    TA1CCTL1 = OUTMOD_2; //Set output mode to toggle/reset on TA1.1; see Family Users Guide
+	TA1CCTL1 = OUTMOD_2; //Set output mode to toggle/reset on TA1.1; see Family Users Guide
         //Configure TA1. Select SMCLK at 1 MHz, divide it by 2, turn it on, no interrupt
         TA1CTL = TASSEL__SMCLK + ID_1 + MC_1 + TACLR;
 
